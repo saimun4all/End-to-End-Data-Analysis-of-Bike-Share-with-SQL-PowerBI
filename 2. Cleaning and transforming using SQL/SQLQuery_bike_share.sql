21f355bfeea710382d5a6723c1ae08ffb6484759
Saimun Hassan <saimunhas@gmail.com>
2024-06-02 19:37:10 -0400

    Added daily revenue and profit in query

--- 2. Cleaning and transforming using SQL/SQLQuery_bike_share.sql
+++ 2. Cleaning and transforming using SQL/SQLQuery_bike_share.sql
@@ -1,13 +1,29 @@
 --1. Append two tables for bike share for 2021 and 2022
 
-WITH bkShare AS(
-SELECT * FROM bike_share_yr_0
+WITH bkShare AS (
+SELECT * FROM bike_data.dbo.bike_share_yr_0
 UNION ALL
-SELECT * FROM bike_share_yr_1)
+SELECT * FROM bike_data.dbo.bike_share_yr_1)
 
 --2. Inner JOIN the bike share table with cost table
+--3. Calculated Revenue and Profit
 
-SELECT * FROM bkShare b
-LEFT JOIN cost_table c
+SELECT  
+	dteday,
+	season,
+	b.yr,
+	b.weekday,
+	hr,
+	rider_type,
+	riders,
+	c.price,
+	COGS, 
+	riders*price AS revenue,
+	riders*price - COGS As profit
+
+FROM bkShare b
+LEFT JOIN bike_data.dbo.cost_table c
 ON b.yr = c.yr 
+
+
 

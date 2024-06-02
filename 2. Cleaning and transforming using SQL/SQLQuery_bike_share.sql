ce10610990dbc17ffc508bd38c48c297f15d2cf9
Saimun Hassan <saimunhas@gmail.com>
2024-06-02 16:32:14 -0400

    Performed left join with the cost table

--- 2. Cleaning and transforming using SQL/SQLQuery_bike_share.sql
+++ 2. Cleaning and transforming using SQL/SQLQuery_bike_share.sql
@@ -1,5 +1,13 @@
---Append two tables for bike share for 2021 and 2022
+--1. Append two tables for bike share for 2021 and 2022
 
+WITH bkShare AS(
 SELECT * FROM bike_share_yr_0
-UNION
-SELECT * FROM bike_share_yr_1
+UNION ALL
+SELECT * FROM bike_share_yr_1)
+
+--2. Inner JOIN the bike share table with cost table
+
+SELECT * FROM bkShare b
+LEFT JOIN cost_table c
+ON b.yr = c.yr 
+
